beea75d617a54601372f2812a40d97d0f26b2211
not_joon <lbj199874@gmail.com>
2023-09-27 00:34:22 +0900

    add test

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ struct BinOpApp {
     command: BinOpCommand,
 }
 
-#[derive(Subcommand, Debug)]
+#[derive(Subcommand, Debug, PartialEq)]
 enum BinOpCommand {
     #[clap(about = "Add two numbers")]
     Add {
@@ -50,4 +50,45 @@ fn string_to_numbers(numbers: String) -> Vec<i32> {
         .into_iter()
         .map(|n| n.parse::<i32>().unwrap())
         .collect::<Vec<i32>>()
+}
+
+#[cfg(test)]
+mod binop_test {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        let app = BinOpApp::parse_from(&["binop", "add", "-n", "1 2 3 4 5"]);
+        assert_eq!(app.command, BinOpCommand::Add { numbers: Some(String::from("1 2 3 4 5")) });
+    }
+
+    #[test]
+    fn test_sub() {
+        let app = BinOpApp::parse_from(&["binop", "sub", "-n", "1 2 3 4 5"]);
+        assert_eq!(app.command, BinOpCommand::Sub { numbers: Some(String::from("1 2 3 4 5")) });
+    }
+
+    #[test]
+    fn test_add_no_numbers() {
+        let app = BinOpApp::parse_from(&["binop", "add"]);
+        assert_eq!(app.command, BinOpCommand::Add { numbers: None });
+    }
+
+    #[test]
+    fn test_string_to_numbers() {
+        let numbers = String::from("1 2 3 4 5");
+        let num_vec = string_to_numbers(numbers);
+
+        assert_eq!(num_vec, vec![1, 2, 3, 4, 5]);
+    }
+
+    #[test]
+    fn test_sum_parsed_numbers() {
+        let numbers = String::from("1 2 3 4 5");
+        let num_vec = string_to_numbers(numbers);
+
+        let sum = num_vec.into_iter().sum::<i32>();
+
+        assert_eq!(sum, 15);
+    }
 }
